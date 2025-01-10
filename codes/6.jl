9c7b72ceb50d3e09d7c86d5503e9b6530a8ec585
y-matsuno525 <162308828+y-matsuno525@users.noreply.github.com>
2025-01-10 10:16:58 +0900

    1/10

--- codes/6.jl
+++ codes/6.jl
@@ -1,94 +1,85 @@
-#=
-計算の流れ
-1. DMRGで(3.17)の十分過去での基底状態を計算する
-
-やること
-1. (3.17)のハミルトニアンを作る
-=#
-
 using ITensors, ITensorMPS
-
 let
+    #基本設定
+    L = 100 #サイト数
+    ε = 1 #格子間距離
+    sites = siteinds("Fermion", L) #サイトの定義
+    t = -10^24
+    m = 1
+
+    #計量に出てくる関数の定義
+    a2 = 2
+    a1 = 1
+    Δt = 1
     
-  N = 100
-  sites = siteinds("S=1/2",N)
-  ζ = 0
-  m = 1
-  ε = 1
-
-
-    
-  #空間微分関数の定義（中央差分,最終サイトがN-1）,そもそもこの差分のとり方でいいか？
-  function dif(f, n, t = nothing);
-    h = 1e-5
-
-    #時間を引数に持つ場合
-    if !t == nothing
-        if n == 0
-            return (f(n + 1,t) - f(n ,t)) / h
-        elseif n = N-1
-            return (f(n ,t) - f(n - 1,t)) / h
-        else
-            return (f(n + 1,t) - f(n - 1,t)) / (2 * h)
-        end
-    #時間を引数に持たない場合
-    else
-        if n == 0
-            return (f(n + 1) - f(n) / h
-        elseif n = N-1
-            return (f(n) - f(n - 1)) / h
-        else
-            return (f(n + 1) - f(n - 1)) / (2 * h)
-        end
+    function α(t, j);
+        return (a2+a1)/2+(a2-a1)/2*tanh(t/Δt)
     end
-  end
 
+    function β(t, j);
+        return 0
+    end
 
-    
-  #計量内関数 α,β,γ　の定義
-  #α = 0.1 * x + t
-　function α(n,t);
-        return 0.1 * (n * ε) + t
-  end
-  #β = 0.05 * x + t
-  function β(n,t);
-        return 0.05 * (n * ε) + t
-  end
-  #γ = 0.075 * x + t
-  function γ(x,t);
-        return 0.075 * (n * ε) + t
-  end
+    function γ(t, j);
+        return (a2+a1)/2+(a2-a1)/2*tanh(t/Δt)
+    end
 
-  function p(n,t);
-        return α(n,t)/γ(n,t)
-  end
+    function p(t, j);
+        return 1
+    end
 
+    function ζ(t, j);
+        return 0
+    end
 
-    
-  #ハミルトニアン(ζは定数)
-  function H_t(N,t);
+    #空間微分用関数
+    function dif(f, t, j);
+        return (f(t, (j+1)*ε)-f(t, (j-1)*ε))/(2*ε)
+    end
+
+    #Localハミルトニアンの定義
+    function hamiltonian(t)
+        os += -1/(2*ε)*α(t, j)/γ(t, j)*cos(ζ(t, j)), "C", j+1, "C", j
+        os += -1/(2*ε)*α(t, j)/γ(t, j)*cos(ζ(t, j)), "Cdag", j, "Cdag", j+1
+        os += -1/(2*ε)*im*α(t, j)/γ(t, j)*sin(ζ(t, j)), "C", j+1, "C", j
+        os -= -1/(2*ε)*im*α(t, j)/γ(t, j)*sin(ζ(t, j)), "Cdag", j, "Cdag", j+1
+        os += -1/(2*ε)*p(t, j), "Cdag", j, "C", j+1
+        os += -1/(2*ε)*p(t, j), "Cdag", j+1, "C", j
+        os += -1/(2*ε)*im*β(t, j), "Cdag", j, "C", j+1
+        os -= -1/(2*ε)*im*β(t, j), "Cdag", j+1, "C", j
+        os += -1/(2*ε)*1/2*(2*p(t, j)-ε*(dif(p, t, j))+2*m*α(t, j)-dif(ζ, t, j)-β(t, j)*dif(ζ, t, j)), "Id", j
+        os += -1/(2*ε)*1/2*(2*p(t, j)-ε*(dif(p, t, j))+2*m*α(t, j)-dif(ζ, t, j)-β(t, j)*dif(ζ, t, j))*(-2), "Cdag", j, "C", j
+        
+    #DMRG
+    #Globalハミルトニアンを生成
     os = OpSum()
-    for j=1:N-1
-      os += (α(j,t)/γ(j,t)*cos(ζ) + p(j,t)) * "X",j,"X",j+1
-      os += -(α(j,t)/γ(j,t)*cos(ζ) - p(j,t)) * "Y",j,"Y",j+1
-      os += -(β(j,t) + α(j,t)/γ(j,t)*sin(ζ)) * "X",j,"X",j+1
-      os += (β(j,t) - α(j,t)/γ(j,t)*sin(ζ)) * "Y",j,"Y",j+1
-      os += (2*p(j,t)-ε(dif(p(j,t),j * ε,t) + 2 * m * α(j,t))) * "Z",j
+    for j=1:L-1
+        os += hamiltonian(j, t)
     end
     H = MPO(os,sites)
-    return H
-  end
+        
+    #初期状態をランダムに生成
+    psi0 = random_mps(sites;linkdims=10)
 
+    nsweeps = 5
+    maxdim = [10,20,100,100,200]
+    cutoff = [1E-10]
 
-    
-  #DMRG実行
-  psi0 = random_mps(sites;linkdims=10)
+    energy,psi = dmrg(H,psi0;nsweeps,maxdim,cutoff)
 
-  nsweeps = 5
-  maxdim = [10,20,100,100,200]
-  cutoff = [1E-10]
-　
-  energy,psi = dmrg(H,psi0;nsweeps,maxdim,cutoff)
+    ###時間発展###
+    cutoff = 1E-8
+    tau = 0.1
+    ttotal = 5.0
 
-  return
+    #gates作成
+    function create_gates(t)
+        gates = ITensor[]
+        for j in 1:(L-1)
+            hj = hamiltonian(t, j)
+            Gj = exp(-im * tau / 2 * hj)
+            push!(gates, Gj)
+        end
+
+    return
 end
