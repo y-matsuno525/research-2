e8796339b70e901e6aa1a14294623d38751dadde
y-matsuno-u <chyu24003@g.nihon-u.ac.jp>
2025-01-11 17:24:01 +0900

    1/11

--- codes/6.jl
+++ codes/6.jl
@@ -1,29 +1,32 @@
 using ITensors, ITensorMPS
+using Plots
+
 let
     ###基本設定#########################################################
     L = 100 #サイト数
     ε = 1 #格子間距離
     sites = siteinds("S=1/2", L) #サイトの定義
-    t = -10^24
+    t = 0
     m = 1
     τ = 0.1 #時間ステップの間隔
-    ttotal = 5.0　#終時刻
+    ttotal = 5　#終時刻
 
     ###計量に出てくる関数の定義##########################################
     a2 = 2
     a1 = 1
     Δt = 1
+    x_h = 50.5
     
     function α(t, j);
-        return (a2+a1)/2+(a2-a1)/2*tanh(t/Δt)
+            return 1#(a2+a1)/2+(a2-a1)/2*tanh(t/Δt)
     end
 
     function β(t, j);
-        return 0
+        return √((x_h-2*t)*ε/(j*ε))
     end
 
     function γ(t, j);
-        return (a2+a1)/2+(a2-a1)/2*tanh(t/Δt)
+        return -1
     end
 
     function p(t, j);
@@ -55,19 +58,21 @@ let
         hj_tmp -= -1/(4*ε)*(α(t, j)/γ(t, j)*cos(ζ(t, j))-p(t, j)) * op("Sy", sites[j]) * op("Sy", sites[j+1])
         hj_tmp -= -1/(4*ε)*(β(t, j)+α(t, j)/γ(t, j)*sin(ζ(t, j))) * op("Sx", sites[j]) * op("Sy", sites[j+1])
         hj_tmp += -1/(4*ε)*(β(t, j)-α(t, j)/γ(t, j)*sin(ζ(t, j))) * op("Sy", sites[j]) * op("Sx", sites[j+1])
-        hj_tmp += -1/(4*ε)*(2*p(t, j)-ε*(dif_x(p, t, j)+2*m*α(t, j)-dif_t(ζ, t, j)-β(t, j)*dif_x(ζ, t, j))) * op("Sz", sites[j]) 
+        hj_tmp += -1/(4*ε)*(2*p(t, j)-ε*(dif_x(p, t, j)+2*m*α(t, j)-dif_t(ζ, t, j)-β(t, j)*dif_x(ζ, t, j))) * op("Sz", sites[j]) *op("Id", sites[j+1])
         return hj_tmp
     end
         
     ###その他関数#############################################################3
     #gates作成
     function create_gates(t)
-        gates = ITensor[]
+        gates_tmp = ITensor[]
         for j in 1:(L-1)
             hj = hamiltonian_op(t, j)
             Gj = exp(-im * τ / 2 * hj)
-            push!(gates, Gj)
+            push!(gates_tmp, Gj)
         end
+        append!(gates_tmp, reverse(gates_tmp))
+        return gates_tmp
     end
 
     #空間微分用関数
@@ -87,9 +92,11 @@ let
     #Globalハミルトニアンを生成
     os = OpSum()
     for j=1:L-1
-        os += hamiltonian_opsum(os, t, j)
+        os += hamiltonian_opsum(t, j)
     end
     H = MPO(os,sites)
+
+    println("ハミルトニアン生成完了")
         
     #初期状態をランダムに生成
     psi0 = random_mps(sites;linkdims=10)
@@ -99,15 +106,18 @@ let
     maxdim = [10,20,100,100,200]
     cutoff_DMRG = [1E-10]
 
-    energy,psi = dmrg(H,psi0;nsweeps,maxdim,cutoff_DMRG)
+    energy,psi = dmrg(H,psi0;nsweeps,maxdim,cutoff=cutoff_DMRG)
+
+    println("DMRG完了")
 
     #TEBDで時間発展を計算
+    sites = s = siteinds(psi) #参考：https://itensor.org/support/3645/the-time-evolution-for-a-h5-file
     Sx_t = []
-    cutoff_TEBD = 1E-8
-    for i,t in enumerate(0.0:τ:ttotal)
+    cutoff_TEBD = 1E-20
+    for (i,t) in enumerate(0.0:τ:ttotal)
         Sx_n = []
         #Sxの期待値をearly timeの基底状態をtまで時間発展させたもので挟む
-        for j in 1:(L-1)
+        for j in 1:L
             Sx = expect(psi, "Sx"; sites=j)
             push!(Sx_n, Sx)
         end
@@ -115,10 +125,22 @@ let
 
         gates = create_gates(t)
 
-        psi = apply(gates, psi; cutoff_TEBD)
+        psi = apply(gates, psi; cutoff=cutoff_TEBD)
         normalize!(psi)
+        println(string(round(i/(ttotal/τ)*100, digits=1))*"%完了")
     end
 
+    println("時間発展完了")
+
     ###プロット#################################################################
+
+    anim = Animation()
+    sites_array = collect(Float32, 1:L)
     
+    for (i, sx_n) in enumerate(Sx_t)
+        plt = plot(sites_array, sx_n,ylim=(-0.5, 0.5) , title="t = "*string(round(i*τ, digits=1)), label="")
+        frame(anim, plt)
+    end
+
+    gif(anim, "plot.gif", fps=20)
 end
